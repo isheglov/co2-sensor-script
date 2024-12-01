93cf5895f31fac45545b30b2ba561cffab5f8e1d
Ilia Shcheglov <ish@novatec-gmbh.de>
2024-12-01 20:56:33 +0100

    Added read from last day table

--- create_db.py
+++ create_db.py
@@ -14,16 +14,16 @@ CREATE TABLE IF NOT EXISTS sensor_data (
     co2 INTEGER NOT NULL,
     temperature REAL NOT NULL,
     humidity REAL NOT NULL
-)
-
+);
+''')
+cursor.execute('''
 CREATE TABLE IF NOT EXISTS last_day_sensor_data (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     date TEXT NOT NULL,
     co2 INTEGER NOT NULL,
     temperature REAL NOT NULL,
     humidity REAL NOT NULL
-)
-
+);
 ''')
 conn.commit()
 conn.close()
